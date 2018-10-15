3d9dd2db00ebb48be2f2dadaa4e710472043fa4c
Rodrigo Amaral <rodrigoamaral@gmail.com>
2018-10-15 12:52:19 -0300

    Get the remaining effort for a task

--- dynapsp/project.py
+++ dynapsp/project.py
@@ -1,15 +1,32 @@
+import networkx as nx
+
 from dynapsp.core import Entity, Repository
 
 class Task(Entity):
-    def __init__(self, oid):
+    def __init__(self, oid, initial_effort=0):
         Entity.__init__(self, oid)
         self._skills = set()
+        self._effort = initial_effort
+        self._finished_effort = 0
 
     
     @property
     def skills(self):
         return self._skills
     
+
+    @property
+    def effort(self):
+        return self._effort
+
+    
+    @property
+    def remaining_effort(self):
+        remaining = self._effort - self._finished_effort
+        if remaining < 0:
+            return 0
+        return remaining
+
     
     def add_skill(self, skill):
         self.skills.add(skill)
@@ -17,6 +34,10 @@ class Task(Entity):
 
     def missing_skills(self, *skillset):
         return self.skills.difference(*skillset)
+
+
+    def add_finished_effort(self, finished_effort):
+        self._finished_effort += finished_effort
 
 
 
@@ -37,18 +58,43 @@ class Project():
     def __init__(self):
         self._tasks = Repository()
         self._employees = Repository()
+        self._tpg = nx.DiGraph()
+
 
     @property
     def tasks(self):
         return self._tasks
 
+
     @property
     def employees(self):
         return self._employees
 
+
+    @property
+    def tpg(self):
+        return self._tpg
+
+
     def add_task(self, task):
         self.tasks.add(task)
-    
+
+
     def add_employee(self, employee):
         self.employees.add(employee)
+
+
+    def add_dependency(self, t1, t2):
+        for t in [t1, t2]:
+            if not self.tasks.get(t):
+                raise InvalidDependencyError("Task {} does not exist".format(t))
+        self.tpg.add_edge(t1, t2)
+
+
+class Error(Exception):
+    pass
+
+
+class InvalidDependencyError(Error):
+    pass
 
--- tests/test_dynapsp.py
+++ tests/test_dynapsp.py
@@ -2,7 +2,7 @@
 """Tests for `dynapsp` package."""
 
 import pytest
-from dynapsp.project import Project, Task, Employee
+from dynapsp.project import Project, Task, Employee, InvalidDependencyError
 
 
 @pytest.fixture
@@ -23,6 +23,13 @@ def employee():
     e.add_skill(2)
     e.add_skill(3)
     return e
+
+@pytest.fixture
+def minimum_project():
+    p = Project()
+    p.add_task(Task(1))
+    p.add_task(Task(2))
+    return p
 
 
 def test_create_project():
@@ -64,4 +71,28 @@ def test_task_missing_skills_multiple(task):
 
 def test_task_employee_missing_skills_(task, employee):
     assert task.missing_skills(employee.skills) == {4, 5}
+
+
+def test_add_dependency_to_tpg(minimum_project):
+    p = minimum_project
+    p.add_dependency(1, 2)
+    assert (p.tpg.number_of_nodes() == 2) and (p.tpg.number_of_edges() == 1) and ((1, 2) in p.tpg.edges)
+
+
+def test_add_dependency_with_missing_task(minimum_project):
+    p = minimum_project
+    with pytest.raises(InvalidDependencyError):
+        p.add_dependency(1, 3)
+
+
+def test_task_add_finished_effort():
+    t = Task(1, initial_effort=10)
+    t.add_finished_effort(2)
+    assert t.remaining_effort == 8
+
+
+def test_task_add_finished_effort_greater_than_remaining():
+    t = Task(1, initial_effort=10)
+    t.add_finished_effort(13)
+    assert t.remaining_effort == 0
 
